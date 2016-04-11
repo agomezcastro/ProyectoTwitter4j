08aba5d370659be3bca011fa0498d5523c8d1e8e
agomezcastro <agomezcastro@atanasoff04.danielcastelao.org>
2016-04-11 09:00:12 +0200

    inicial

---
+++ src/proyectotwitter4j/ProyectoTwitter4j.java
@@ -0,0 +1,17 @@
+package proyectotwitter4j;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author agomezcastro
+ */
+public class ProyectoTwitter4j {
+
+
+    public static void main(String[] args) {
+        Twitter mitwitter = new TwitterFactory().getInstance();
+    }
+    
+}
